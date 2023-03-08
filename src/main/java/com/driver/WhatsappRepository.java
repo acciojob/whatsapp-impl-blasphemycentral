d7779ae1f22d59b2666967ca0e6ba6dd1cc9cde2
blasphemycentral <aayushnag84@gmail.com>
2023-03-08 17:46:35 +0530

    solution 6

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -89,7 +89,7 @@ public class WhatsappRepository {
     public String changeAdmin(User approver, User user, Group group) throws Exception
     {
         if(!groupUserMap.containsKey(group)) throw new Exception("Group does not exist");
-        else if(!group.getAdmin().equals(approver.getMobile())) throw new Exception("Approver does not have rights");
+        else if(!adminMap.get(group).getMobile().equals(approver.getMobile())) throw new Exception("Approver does not have rights");
         else if(!groupUserMap.get(group).contains(user)) throw new Exception("User is not a participant" );
         else
         {
